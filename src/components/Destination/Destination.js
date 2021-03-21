824bc0fb91edc99630ff6786600d7aa6a72ae8eb
Naziullah Shawn <naziullahshawn135@gmail.com>
2021-03-21 21:50:42 +0600

    finish

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Destination.css';
-const Destination = () => {
+import Map from '../images/Map.png';
+const Destination = (props) => {
+    const [ride,setRide] = useState(true);
     return (
-        <div>
-            <h1>Ride with</h1>
+        <div className="d-flex">
+            <div className="form-div col-lg-4">
             <div className="search-form">
-                <form action="">
+                {ride ? (<form action="">
+                <h1>Ride with </h1>
                 <label htmlFor="">Pick From</label>
                 <br/>
-                <input type="text" name="" id="" required/>
+                <input className="input-box" type="text" name="" id="" required/>
                 <br/>
                 <label htmlFor="">Pick To</label>
                 <br/>
-                <input type="text" name="" id="" required/>
+                <input className="input-box" type="text" name="" id="" required/>
                 <br/>
-               <button className="btn btn-warning" type="submit">Search</button>
-                </form>
+                <button onClick={() => setRide(false)} className="btn btn-warning search-btn" type="submit">Search</button>
+                </form>):
+                ( <div>
+                    <div className="destination-box">
+                        <div><h4>Mirpur 1</h4></div>
+                        <div><h4>Dhanmondi</h4></div>
+                    </div>
+                    <div>$67</div>
+                    <div>$67</div>
+                    <div>$67</div>
+                </div> ) }                
             </div>
+            
+        </div>
+        <div className="map-box col-lg-7">
+            <img src={Map} alt=""/>
+        </div>
         </div>
     );
 };
